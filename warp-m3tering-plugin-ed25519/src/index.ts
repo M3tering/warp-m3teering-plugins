bffbacfd9d0dcf98e856048e0770b0f0912322d8
ichristwin <ichristwin@gmail.com>
2024-03-16 08:44:37 +0100

    fixed issue with plugins ready to publish

--- warp-m3tering-plugin-ed25519/src/index.ts
+++ warp-m3tering-plugin-ed25519/src/index.ts
@@ -1,9 +1,9 @@
-import { verify } from "@stablelib/ed25519";
+import * as ed25519 from "@stablelib/ed25519";
 import { WarpPlugin, WarpPluginType } from "warp-contracts";
 
 export class Ed25519Extension implements WarpPlugin<any, void> {
   process(input: any): void {
-    input.ed25519.verify = verify;
+    input.ed25519 = ed25519;
   }
 
   type(): WarpPluginType {
--- warp-m3tering-plugin-ethers/src/index.ts
+++ warp-m3tering-plugin-ethers/src/index.ts
@@ -3,8 +3,7 @@ import { WarpPlugin, WarpPluginType } from "warp-contracts";
 
 export class EthersExtension implements WarpPlugin<any, void> {
   process(input: any): void {
-    input.ethers.Interface = ethers.Interface;
-    input.ethers.JsonRpcProvider = ethers.JsonRpcProvider;
+    input.ethers = ethers;
   }
 
   type(): WarpPluginType {
